beb69e5db7c6404107ee8d3f6116858431b48002
marcussenna <marcussena01@gmail.com>
2025-06-17 19:52:47 -0300

    a

---
+++ 06/ex008.js
@@ -0,0 +1,8 @@
+var vel = 50
+console.log (`a velocidade de seu carro e ${vel} km/h`)
+if (vel > 60) { // condição simples!
+    console.log(`voce ultrapassou a velocidade permitida!`)
+}
+if (vel < 60) {
+    console.log(`voce esta digirindo dentro da velocidade permitida!`)
+}
